1f031a3a2b9bd3f9c36ae6e1222a7671ec99ed8a
Crashman <48332956+bassolehermann@users.noreply.github.com>
2019-09-18 18:58:00 +0000

    Add files via upload

---
+++ lib/list.dart
@@ -0,0 +1,68 @@
+
+import 'package:cataloguewidget/Semantics.dart';
+import 'package:cataloguewidget/Semantics.dart' as prefix0;
+
+import 'AnimatedIcon.dart';
+import 'AnimatedList.dart';
+import 'AnimatedOpacity.dart';
+import 'AnimatedPadding.dart';
+import 'AnimatedPositionned.dart';
+import 'AnimatedSwitcher.dart';
+import 'AspestRadion.dart';
+import 'ConstrainedBox.dart';
+import 'Draggable.dart';
+import 'IndexedStack.dart';
+import 'LimitedBox.dart';
+import 'PlaceHolder.dart';
+import 'RichText.dart';
+import 'Stack.dart';
+import 'Spacer.dart';
+import 'ReorderableListview.dart';
+import 'Semantics.dart';
+import 'MediaQuery.dart';
+import 'Flexible.dart';
+import 'Inherited.dart';
+List<String> item = [
+  'AnimetedIcon',
+  'AnimetedList',
+  'Flexible',
+  'AnimetedPadding',
+  'AnimetedOpacity',
+  'AnimetedPositioned',
+  'AnimetedSwitcher',
+  'AspectRadio',
+  'ConstrainedBox',
+  'Draggable',
+  'IndexStack',
+  'InheritedWidget',
+  'LimitedBox',
+  'MediaQuery',
+  'PlaceHolder',
+  'rehorderablelistview',
+  'RichText',
+  'Semantics',
+  'Spacer',
+  'Stack',
+];
+List listwidget = [
+  MyAnimatedIcon(),
+  MyAnimatedList(),
+  MyFlexible(),
+  MyAnimatedPadding(),
+  MyAnimatedOpacity(),
+  MyAnimatedPositioned(),
+  MyAnimatedSwitcher(),
+  MyAspectRadio(),
+  MyConstrainedBox(),
+  MyDraggable(),
+  MyIndexedStack(),
+  MyTree(),
+  MyLimitedBox(),
+  MyMediaQuery(),
+  MyPlaceHolder(),
+  MyReorderableListView(),
+  MyRichText(),
+  MySemantics(),
+  MySpacer(),
+  MyStack(),
+];
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'list.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final GlobalKey debuger=GlobalKey();
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+
+      title: 'Mes Widgets',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Mes Widgets'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Container(
+                child: Text("📲",style: TextStyle(fontSize: 50.0),),
+              ),
+            ),
+            Expanded(
+              flex: 12,
+              child: ListView.builder(
+                  itemCount: item.length,
+                  itemBuilder: (context, i) {
+                    return FlatButton(
+
+                      onPressed: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => listwidget[i],
+                        ),
+                      ),
+                      child: Text(
+                        item[i],style: TextStyle(fontSize: 20.0,),
+                      ),
+                      color: Colors.grey,
+                      padding: EdgeInsets.all(5.0),
+
+                    );
+                  }),
+            )
+          ],
+        )
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
